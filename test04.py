730a164cc2fb904661f92c9c7f290119ab130b4f
chanakan6519410016 <s6519410016@sau.ac.th>
2023-10-20 11:40:58 +0700

    code v.1 :test

--- test04.py
+++ test04.py
@@ -22,3 +22,30 @@ print(my_list1[:3]) # 10, 20, True
 
 
 # เข้าถึงทุกข้อมูล
+for info in my_list1 :
+    print(info, end=', ')
+    
+print()
+
+print(my_list1)
+my_list1[4] = 'Thailand'
+print(my_list1)
+
+# List Method
+my_list2 = [10, 20, True, 10, 'SAU', [20, 'IoT']]
+my_list2.append('wow')
+print(my_list2)
+my_list2.append([111,222,333])
+print(my_list2)
+my_list2.extend([444,555])
+print(my_list2)
+my_list2.remove(10)
+my_list2.remove('SAU')
+my_list2.remove([111,222,333])
+print(my_list2)
+my_list2.pop()
+my_list2.pop()
+my_list2.pop()
+print(my_list2)
+my_list2.pop(2) # 2 คือ index
+print(my_list2)
